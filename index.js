a1e669aae6ff02ec436015f4100fcd71af80e7c3
Ini Mueller <mail@inimueller.com>
2020-10-27 11:27:28 +0100

    code from encounter part 2

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 
+//boilerplate code from the notes "Image Upload" //
+
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
+
+//// end of boilerplate ////
+
 app.use(express.static("public"));
 //it will look for an index.html file in the public folder and if there is, it will render that
 
@@ -10,6 +36,24 @@ app.get("/images", (req, res) => {
         // console.log(rows);
         res.json(rows);
     });
+});
+
+app.post("/upload", uploader.single("file"), (req, res) => {
+    // when I make a post request, before it runs the code, I want it to upload to our hard drive.
+    // hence why we need the uploader thing from multer.
+    // this means: ADD it to my uploads folder
+    // multer add the file and the body to the request object!
+    // console.log("input values: ", req.body);
+    // console.log("file: ", req.file);
+    if (req.file) {
+        res.json({
+            success: true,
+        });
+    } else {
+        res.json({
+            success: false,
+        });
+    }
 });
 
 app.listen(8000, () => console.log("Ini's server up and running"));
--- public/js/script.js
+++ public/js/script.js
@@ -3,12 +3,45 @@ new Vue({
     el: "#main",
     data: {
         images: [],
+        //data properties self will store values of imput fields
+        title: " ",
+        description: " ",
+        username: " ",
+        file: null,
     },
     mounted: function () {
-        let that = this;
+        let self = this;
         axios.get("/images").then(function (response) {
-            that.images = response.data;
+            self.images = response.data;
         });
     },
-    methods: {},
+
+    // ALL the functions self I create are defined in METHODS!
+    methods: {
+        handleClick: function (e) {
+            e.preventDefault();
+            // console.log("this: ", this); // in this case this refers to data instances
+
+            let formData = new FormData();
+            formData.append("title", this.title);
+            formData.append("description", this.description);
+            formData.append("username", this.username);
+            formData.append("file", this.file);
+
+            axios
+                .post("/upload", formData)
+                .then(function (res) {
+                    console.log("res from POST /upload", res);
+                })
+                .catch(function (err) {
+                    console.log("err in POST /upload", err);
+                });
+        },
+
+        handleChange: function (e) {
+            console.log("handleChange is running!");
+            console.log("file: ", e.target.files[0]);
+            this.file = e.target.files[0];
+        },
+    },
 });
